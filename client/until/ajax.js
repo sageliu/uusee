3550d84073d481224cb496031d38665cb15dae3e
liupingyin <liupingyin@xjgreat.com>
2017-08-29 14:26:02 +0800

    o

--- client/until/ajax.js
+++ client/until/ajax.js
@@ -1,3 +1,38 @@
-export default function ajax(method='get',) {
+let ajax = ({ method='', url='', async=true, data={}, headers={} }) => {
+  let xhr = new XMLHttpRequest();
+  xhr.open(method, url, async);
 
+  for ( let key in headers ) {
+    xhr.setRequestHeader(key, headers[key]);
+  }
+  if ( method.toUpperCase() === 'POST' ) {
+    xhr.setRequestHeader('content-type', 'application/json');
+  }
+
+  let sendString = typeof data === 'string' ? data : JSON.stringify(data);
+  xhr.send(sendString);
+
+  return new Promise((resolve, reject) => {
+    xhr.onload = function () {
+      try{
+        resolve(JSON.parse(xhr.responseText));
+      }catch (e){
+        reject(xhr.responseText);
+        console.log(e);
+      }
+    };
+
+    // xhr.onreadystatechange = function () {
+    //   if ( xhr.status === 200 && xhr.readyState === 4 ) {
+    //     resolve(xhr.responseText);
+    //   } else {
+    //     reject(xhr.responseText);
+    //   }
+    // }
+  });
+};
+
+export { ajax }
+export default {
+  ajax
 }
